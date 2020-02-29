6f249acc1ae85fc0c565a24cef161b3d52dec152
Matthew Rocafort <mvrocafort@up.edu.ph>
2020-02-29 23:04:19 +0800

    Added an add item functionality. Fixed consume item(UPDATE) functionality.

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 // Initialize the requirements
 const express = require('express');
 const mysql = require('mysql');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 
 
 // Create express object
 var app = express();
+
+// Set the view engine
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 // Set the port number if there is a specified port
 const port = process.env.PORT || 3000;
@@ -33,30 +39,55 @@ app.get('/items', (req, res) => {
 	con.query('SELECT * FROM items', (err, rows, fields) => {
 		if(!err)
 			//console.log(rows);
-			res.send(rows);
+			//res.send(rows);
+			res.render('home', {items: rows});
+		//else
+			//console.log(err);
+	})
+});
+
+// Consume an item
+app.get('/items/consume/:id', (req, res) => {
+	const id = req.params.id;
+	con.query(`UPDATE items SET items.quantity = items.quantity - 1 WHERE items.id = ${id}`,(err, rows, fields) => {
+		if(!err)
+			//console.log(rows);
+			res.redirect('/items');
 		else
 			console.log(err);
 	})
 });
 
-
-// Delete an item
-app.delete('/items/delete/:id', (req, res) => {
-	con.query('DELETE FROM items WHERE id = ?', [req.params.id],(err, rows, fields) => {
+// Delete item from inventory
+app.get('/items/delete/:id', (req, res) => {
+	const id = req.params.id;
+	con.query(`DELETE FROM items WHERE items.id = ${id}`,(err, rows, fields) => {
 		if(!err)
 			//console.log(rows);
-			res.send('Deleted!');
+			res.redirect('/items');
 		else
 			console.log(err);
 	})
 });
+ 
 
-// Consume an item (this is an UPDATE function)
-app.put('/items/consume/:name', (req, res) => {
-	con.query(`UPDATE items SET items.quantity = items.quantity - 1 WHERE items.name = ?`, [req.params.name],(err, rows, fields) => {
+// Show add item form to inventory system
+app.get('/items/add/', (req, res) => {
+    res.render('insert');
+});
+
+// Add item to inventory system
+app.post('/save', (req, res) => {
+    let data = {
+    	name: req.body.name,
+    	quantity: req.body.quantity,
+    	amount: req.body.amount
+    };
+    let sql = "INSERT INTO items SET ?";
+	con.query(sql, data, (err, rows, fields) => {
 		if(!err)
 			//console.log(rows);
-			res.send(rows);
+			res.redirect('/items');
 		else
 			console.log(err);
 	})
