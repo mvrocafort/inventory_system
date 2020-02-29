faece75b1d598fd9e25f56ca9d0b6911bf6f3e23
Matthew Rocafort <mvrocafort@up.edu.ph>
2020-02-29 18:49:06 +0800

    Added a view all items function.

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
-var mysql = require('mysql');
+// Initialize the requirements
+const express = require('express');
+const mysql = require('mysql');
+const bodyparser = require('body-parser');
 
+
+// Create express object
+var app = express();
+
+// Set the port number if there is a specified port
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+	console.log(`Server started on port ${port}.`)
+})
+
+// Create a connection with MySQL
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: ""
+  password: "",
+  database: "inventory"
 });
 
+// Connect
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+});
+
+// Select and display all items
+app.get('/items', (req, res) => {
+	con.query('SELECT * FROM items', (err, rows, fields) => {
+		if(!err)
+			//console.log(rows);
+			res.send(rows);
+		else
+			console.log(err);
+	})
 });
